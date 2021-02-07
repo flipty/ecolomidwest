e3dbf5acc8b7511de4c23fc69ce5d4b7ffe2fdb9
Dave Tesch <d@vetes.ch>
2021-02-07 14:11:25 -0600

    add base plugins and additional multisite config

---
+++ wp-content/themes/ecolo/footer.php
@@ -0,0 +1,12 @@
+<footer>
+  3223 East Lake Street, Minneapolis, Minnesota 55406 <a href="tel:+16125016929">(612)-501-6929</a> <?php the_field('footer_hours', 'options');?>
+</footer>
+
+<script src="<?php echo get_template_directory_uri(); ?>/js/vendor/modernizr-3.8.0.min.js"></script>
+<script src="<?php echo get_template_directory_uri(); ?>/js/vendor/jquery-3.4.1.min.js"></script>
+<script src="<?php echo get_template_directory_uri(); ?>/js/vendor/owl.carousel.min.js"></script>
+<script src="<?php echo get_template_directory_uri(); ?>/js/habanero.js"></script>
+<?php wp_footer(); ?>
+
+</body>
+</html>
